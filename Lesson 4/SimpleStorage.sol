02a83c47e78e59afc9630232468ba375583f8759
Jose Pina <pinajoma0@gmail.com>
2021-12-02 14:23:01 +0100

    Start lesson 4

--- Lesson 4/SimpleStorage.sol
+++ Lesson 4/SimpleStorage.sol
@@ -1,9 +1,12 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
+
     uint256 favoriteNumber;
 
+    // This is a comment!
     struct People {
         uint256 favoriteNumber;
         string name;
@@ -12,20 +15,16 @@ contract SimpleStorage {
     People[] public people;
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) external {
+    function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
-
-    function retrieve() public view returns (uint256) {
+    
+    function retrieve() public view returns (uint256){
         return favoriteNumber;
     }
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-
-    function addOtherNumner(uint256 _favoriteNumber2) public {
-        this.store(_favoriteNumber2);
     }
 }
