e5d2eb39a09c62ad6064382024970178d6a79657
Jose Pina <pinajoma0@gmail.com>
2021-12-02 15:33:01 +0100

    deploy simpleStorage with brownie

---
+++ Lesson 5/scripts/deploy.py
@@ -0,0 +1,24 @@
+from typing import NewType
+from brownie import accounts, config, SimpleStorage, network
+
+
+def deploy_simple_storage():
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(20, {"from": account})
+    transaction.wait(1)
+    update_stored_value = simple_storage.retrieve()
+    print(update_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def main():
+    deploy_simple_storage()
